3774ba30433f3028592a6017f0399faca78b1db2
Belal Uddin <belal1@hotmail.co.uk>
2024-06-12 20:23:16 +0100

    Refactoring

--- src/types/job.ts
+++ src/types/job.ts
@@ -1,4 +1,5 @@
 import { getCell, getSheet } from "../helpers/helpers.excel";
+import { IArrayDataPrinter } from "./printer";
 
 export interface IJob {
     run(context: Excel.RequestContext): Promise<boolean>;
@@ -34,24 +35,14 @@ export class CleanUpJob implements IJob {
                 callerCell.load();
                 await context.sync();
 
-                // ToDo: Do ignore case and whitespace comparison
+                // ToDo: Implement case insensitive and whitespace free comparison
                 if (callerCell.formulas[0][0] === this.CallerCellOriginalFormula) {
-                    let sheet: Excel.Worksheet;
-
-                    try {
-                        sheet = getSheet(this.Invocation.address, context);
-                    }
-                    catch {
-                        // Sheet no longer exists
-                        return true;
-                    }
-
                     if (this.ArrayDataRowsCount > 1) {
-                        sheet.getRangeByIndexes(callerCell.rowIndex + 1, callerCell.columnIndex, this.ArrayDataRowsCount - 1, this.ArrayDataColsCount).clear(Excel.ClearApplyTo.contents);
+                        callerCell.worksheet.getRangeByIndexes(callerCell.rowIndex + 1, callerCell.columnIndex, this.ArrayDataRowsCount - 1, this.ArrayDataColsCount).clear(Excel.ClearApplyTo.contents);
                     }
 
                     if (this.ArrayDataColsCount > 1) {
-                        sheet.getRangeByIndexes(callerCell.rowIndex, callerCell.columnIndex + 1, this.ArrayDataRowsCount, this.ArrayDataColsCount - 1).clear(Excel.ClearApplyTo.contents);
+                        callerCell.worksheet.getRangeByIndexes(callerCell.rowIndex, callerCell.columnIndex + 1, this.ArrayDataRowsCount, this.ArrayDataColsCount - 1).clear(Excel.ClearApplyTo.contents);
                     }
 
                     await context.sync();
@@ -66,8 +57,48 @@ export class CleanUpJob implements IJob {
     }
 }
 
-// export class PrintJob implements IJob {
-//     public run(context: Excel.RequestContext): Promise<boolean> {
-        
-//     }
-// }
+export class PrintJob implements IJob {
+    private CallerCellOriginalFormula: any;
+    private ArrayData: any[];
+    private ArrayDataPrinter: IArrayDataPrinter;
+    private Invocation: CustomFunctions.Invocation;
+
+
+    public constructor(callerCellOriginalFormula: any, arrayData: any[], arrayDataPrinter: IArrayDataPrinter, invocation: CustomFunctions.Invocation) {
+        this.CallerCellOriginalFormula = callerCellOriginalFormula;
+        this.ArrayData = arrayData;
+        this.ArrayDataPrinter = arrayDataPrinter;
+        this.Invocation = invocation;
+    }
+    
+    public async run(context: Excel.RequestContext): Promise<boolean> {
+        try {
+            if (context && this.Invocation && this.Invocation.address && this.CallerCellOriginalFormula && this.ArrayData && this.ArrayData.length > 0 && this.ArrayDataPrinter) {
+                let callerCell: Excel.Range;
+                
+                try {
+                    callerCell = getCell(this.Invocation.address, context);
+                }
+                catch {
+                    // Caller cell no longer exists
+                    return true;
+                }
+
+                callerCell.load();
+                await context.sync();
+
+                // ToDo: Implement case insensitive and whitespace free comparison
+                if (callerCell.formulas[0][0] === this.CallerCellOriginalFormula) {
+                    if (await this.ArrayDataPrinter.print(callerCell, this.ArrayData)) {
+                        await context.sync();
+                    }
+                }
+            }
+
+            return true;
+        }
+        catch {
+            return false;
+        }
+    }
+}
---
+++ src/types/printer.ts
@@ -0,0 +1,45 @@
+export interface IArrayDataPrinter {
+    print(callerCell: Excel.Range, arrayData: any[]): Promise<boolean>;
+}
+
+export class ArrayDataVerticalPrinter implements IArrayDataPrinter {
+    public async print(callerCell: Excel.Range, arrayData: any[]): Promise<boolean> {
+        try {
+            if (callerCell && arrayData && arrayData.length > 0) {
+                const arrayDataForPrint: any[] = [];
+    
+                for (let i = 0; i < arrayData.length; i++) {
+                    arrayDataForPrint.push([arrayData[i]]);
+                }
+        
+                callerCell.worksheet.getRangeByIndexes(callerCell.rowIndex + 1, callerCell.columnIndex, arrayDataForPrint.length, 1).values = arrayDataForPrint;
+            }
+
+            return true;
+        }
+        catch {
+            return false;
+        }
+    }
+}
+
+export class ArrayDataHorizontalPrinter implements IArrayDataPrinter {
+    public async print(callerCell: Excel.Range, arrayData: any[]): Promise<boolean> {
+        try {
+            if (callerCell && arrayData && arrayData.length > 0) {
+                // const arrayDataForPrint: any[] = [];
+    
+                // for (let i = 1; i < values.length; i++) {
+                //     arrayDataForPrint.push(values[i]);
+                // }
+
+                callerCell.worksheet.getRangeByIndexes(callerCell.rowIndex, callerCell.columnIndex + 1, 1, arrayData.length).values = [arrayData];
+            }
+
+            return true;
+        }
+        catch {
+            return false;
+        }
+    }
+}
