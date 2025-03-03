47aaf0bc0b94067d53c171d2a71da89d2a43c0ca
Belal Uddin <belal1@hotmail.co.uk>
2025-03-03 18:04:04 +0000

    Restores default state.

--- src/services/weather/weather.result.service.ts
+++ src/services/weather/weather.result.service.ts
@@ -44,6 +44,7 @@ export class WeatherResult implements IWeatherResultService {
         } else {
             if (!this.Fields?.length) {
                 this.Fields = [new HumidityFieldService(), new PressureFieldService(), new WindDirFieldService()];
+                //this.Fields = [new HumidityFieldService()];
             }
             
             //ToDo: Consider using multiple services
