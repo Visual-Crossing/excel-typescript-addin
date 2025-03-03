f4f0b65ac47c95a7cbf7fa5aba14e80251cc3695
Belal Uddin <belal1@hotmail.co.uk>
2025-03-03 18:05:04 +0000

    Removes commented out lines.

--- src/services/weather/weather.result.service.ts
+++ src/services/weather/weather.result.service.ts
@@ -73,47 +73,4 @@ export class WeatherResult implements IWeatherResultService {
 
         return this.outputArrayData;
     }
-
-    // public toMatrix(): Matrix {
-    //     // if (!this.CurrentFormula || !this.Fields?.length || !this.JsonData || !this.PrintDirection) {
-    //     //     throw new Error();
-    //     // }
-
-    //     if (!this.Fields?.length) {
-    //         this.Fields = [new HumidityFieldService(), new PressureFieldService(), new WindDirFieldService()];
-    //     }
-
-    //     if (!this.CacheItem?.values && !this.CacheItem?.error) {
-    //         return { FormulaCellDisplayValue: PROCESSING };
-    //     }
-
-    //     const outputArrayData: any[][] = [];
-        
-    //     if (this.CacheItem.error) {
-    //         outputArrayData.push([this.CacheItem.error]);
-    //     } else {
-    //         this.IncludeTitle = true;
-    //         //ToDo: Consider using multiple services
-    //         if (this.IncludeTitle) {
-    //             this.Fields.forEach((field) => outputArrayData.push([field.getTitle(), field.getValue(this.CacheItem)]));
-    //         } else {
-    //             this.Fields.forEach((field) => outputArrayData.push([field.getValue(this.CacheItem)]));
-    //         }
-    //     }
-
-    //     const formulaCellDisplayValue: string | number | Date = outputArrayData[0][0];
-
-    //     if (this.CurrentFormula) {
-    //         //ToDo: Use services
-    //         const arrayDataCols = getArrayDataCols(outputArrayData, this.PrintDirection);
-    //         const arrayDataRows = getArrayDataRows(outputArrayData, this.PrintDirection);
-
-    //         const formulaUpdaterService = Container.get<IFormulaUpdaterService>('service.updater.formula');
-    //         outputArrayData[0][0] = formulaUpdaterService.generateUpdatedFormula(this.CurrentFormula, this.CurrentColsRows, arrayDataCols, arrayDataRows)
-    //     }
-
-    //     const matrix: Matrix = { FormulaCellDisplayValue: formulaCellDisplayValue, OutputArrayData: outputArrayData };
-
-    //     return matrix;
-    // }
 }
