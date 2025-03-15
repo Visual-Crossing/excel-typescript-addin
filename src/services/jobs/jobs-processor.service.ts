a954302b5bd12c5ece0dc497759f239472ef4a97
Belal Uddin <belal1@hotmail.co.uk>
2025-03-15 17:52:42 +0000

    Refactoring.

--- src/services/jobs/jobs-processor.service.ts
+++ src/services/jobs/jobs-processor.service.ts
@@ -1,16 +1,12 @@
-import Container from "typedi";
 import { Queue } from "queue-typescript";
-import { IJobService, jobTypes } from "../../types/services/jobs/job.service.type";
+import { IJobService } from "../../types/services/jobs/job.service.type";
 import { IJobsProcessorService } from "../../types/services/jobs/jobs-processor.service.type";
-import { IWeatherResultsStoreService } from "../../types/services/weather.result.store.service.type";
 
 const RETRY_MS: number = 250;
 
 export class JobsProcessorService<T> implements IJobsProcessorService<T> {
     private jobs: Queue<IJobService<T>> | null = null;
     private isJobsProcessingInProgress: boolean = false;
-
-    // private activePrintJobs: Map<string, string | number | Date | null> | null = null;
 
     public add(job: IJobService<T>): void {
         if (!job) {
@@ -20,70 +16,30 @@ export class JobsProcessorService<T> implements IJobsProcessorService<T> {
         if (!this.jobs) {
             this.jobs = new Queue<IJobService<T>>();
         }
-    
-        // if (job.getType() === jobTypes.cleanUp) {
-        //     this.jobs.prepend(job);
-        // } else {
-            this.jobs.enqueue(job);
-        // }
+
+        this.jobs.enqueue(job);
     }
 
-    // public removePrintJob(id: string): void {
-    //     if (!id) {
-    //         return;
-    //     }
-
-    //     if (this.activePrintJobs === null) {
-    //         return;
-    //     }
-
-    //     if (!this.activePrintJobs.has(id)) {
-    //         return;
-    //     }
-
-    //     this.activePrintJobs.delete(id);
-
-    //     if (this.activePrintJobs.size === 0) {
-    //         this.activePrintJobs = null;
-    //     }
-    // }
-
-    // public printJobExists(id: string): boolean {
-    //     if (!id) {
-    //         return false;
-    //     }
-
-    //     if (this.activePrintJobs === null) {
-    //         return false;
-    //     }
-
-    //     return this.activePrintJobs.has(id);
-    // }
+    private retry(): void {
+        const timeout: NodeJS.Timeout = setTimeout(async () => { clearTimeout(timeout); this.process(); }, RETRY_MS);
+    }
 
     public async process(): Promise<void> {
         if (this.jobs && this.jobs.length > 0 && !this.isJobsProcessingInProgress) {
             try {
-                this.isJobsProcessingInProgress = true;
-
-                await Excel.run(async (context: Excel.RequestContext) => {
+                Excel.run(async (context: Excel.RequestContext) => {
                     try {
+                        this.isJobsProcessingInProgress = true;
+
                         while (this.jobs && this.jobs.length > 0) {
                             const job: IJobService<T> = this.jobs.front;
 
                             if (job) {
-                                // if (job .getType() === jobTypes.print) {
-                                //     if (this.activePrintJobs == null) {
-                                //         this.activePrintJobs = new Map<string, string | number | Date>();
-                                //     }
-
-                                //     this.activePrintJobs.set(`${job.key}_${job.getAddress()}`, null);
-                                // }
-
                                 if (await job.run(context)) {
                                     this.jobs.dequeue();
                                 }
                                 else {
-                                    const timeout: NodeJS.Timeout = setTimeout(async () => { clearTimeout(timeout); this.process(); }, RETRY_MS);
+                                    this.retry();
                                     return;
                                 }
                             } else {
@@ -95,7 +51,7 @@ export class JobsProcessorService<T> implements IJobsProcessorService<T> {
                     }
                     catch {
                         if (this.jobs && this.jobs.length > 0) {
-                            const timeout: NodeJS.Timeout = setTimeout(async () => { clearTimeout(timeout); this.process(); }, RETRY_MS);
+                            this.retry();
                             return;
                         }
                     }
@@ -103,20 +59,14 @@ export class JobsProcessorService<T> implements IJobsProcessorService<T> {
                         this.isJobsProcessingInProgress = false;
                     }
                 });
-
-                // if (!this.jobs || this.jobs.length === 0) {
-                //     const weatherResultsStore = Container.get<IWeatherResultsStoreService>('service.results.store.weather');
-                //     weatherResultsStore.clear();
-                // }
             }
             catch {
+                this.isJobsProcessingInProgress = false;
+
                 if (this.jobs && this.jobs.length > 0) {
-                    const timeout: NodeJS.Timeout = setTimeout(async () => { clearTimeout(timeout); this.process(); }, RETRY_MS);
+                    this.retry();
                     return;
                 }
-            }
-            finally {
-                this.isJobsProcessingInProgress = false;
             }
         }
     }
